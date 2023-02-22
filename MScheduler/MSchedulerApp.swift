874a27e1b9d064bb6651c893a5b356ece0aead80
moseoh <azqazq195@gmail.com>
2023-02-23 02:30:52 +0900

    Initial Commit

---
+++ MScheduler/MSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSchedulerApp.swift
+//  MScheduler
+//
+//  Created by 문성하 on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
